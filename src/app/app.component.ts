5dc394c1f17f5b846a19e9a7add13038461c5b14
Sebastian Rojo Londoño <sebastian.rojo@multienlace.com.co>
2019-01-17 17:15:45 -0500

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myfirstapp';
+
+  name: string = "Jhon Carter";
+  age: number = 28;
+  
+  users: string[] = ['ryan', 'joe', 'cameron'];
+  
+  addUser(newUser) {
+    this.users.push(newUser.value);
+    newUser.value = "";
+    newUser.focus();
+    return false;
+  }
+
+  deleteUser(user) {
+    for(let i = 0; i < this.users.length; i++) {
+      if (user == this.users[i]) {
+        this.users.splice(i, 1);
+      }
+    }
+  }
+
 }
