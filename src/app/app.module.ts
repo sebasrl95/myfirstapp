30c3c032f944398c30bc344edcad83bbbbfb886e
Sebastian Rojo <sebasrojo95@gmail.com>
2019-01-17 21:22:54 -0500

    Routing and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { DataService } from './data.service';
+import { AboutComponent } from './about/about.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: UserComponent
+  },
+  {
+    path: 'about',
+    component : AboutComponent
+  },
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { Post } from '../Post';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  @Input() nameUser;
+export class UserComponent {
+  name: string = "Jhon Carter";
+  age: number = 28;
   
-  constructor() { }
-
-  ngOnInit() {
+  users: string[] = ['ryan', 'joe', 'cameron'];
+  posts: Post[];
+  
+  constructor(private dataService: DataService) {
+    this.dataService.getData().subscribe((data) => {
+        this.posts = data;
+    });
   }
 
+  addUser(newUser) {
+    this.users.push(newUser.value);
+    newUser.value = "";
+    newUser.focus();
+    return false;
+  }
+
+  deleteUser(user) {
+    for(let i = 0; i < this.users.length; i++) {
+      if (user == this.users[i]) {
+        this.users.splice(i, 1);
+      }
+    }
+  }
 }
